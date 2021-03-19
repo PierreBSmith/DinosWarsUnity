163bc56f975a493b31821e9176104f1471c8dac7
PierreBSmith <38896073+PierreBSmith@users.noreply.github.com>
2021-03-18 18:06:54 -0700

    no actual code changes, but there is alot of blueprints for skill tree stuff

---
+++ Assets/Scripts/Skills/ClassSpecificSkills/VelociraptorSkills/ArmorBreaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorBreaker : MonoBehaviour
+{
+    //Armor penetration -- attack damage tree velociraptor
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Skills/Generic/QuickFeet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickFeet : MonoBehaviour
+{
+    //Movement stamina is less
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
